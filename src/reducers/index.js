bf3d327eef7a330c7474231e937e75c4d10ccee3
Christian Fleschhut <weltrekordhalter@gmail.com>
2020-01-24 23:29:37 +0100

    Add saga to fetch and add data to redux store

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,11 +1,23 @@
 import { combineReducers } from 'redux';
+import * as TYPES from '../types';
 
 const initialState = {
   people: [],
 };
 
+const handleStarWarsFetchSuccess = (state, action) => ({
+  ...state,
+  people: action.data,
+});
+
 const starWars = (state = initialState, action) => {
-  return state;
+  switch (action.type) {
+    case TYPES.FETCH_STAR_WARS_SUCCESS:
+      return handleStarWarsFetchSuccess(state, action);
+
+    default:
+      return state;
+  }
 };
 
 const rootReducer = combineReducers({
--- src/store/index.js
+++ src/store/index.js
@@ -1,7 +1,16 @@
 import { createStore, applyMiddleware } from 'redux';
+import createSagaMiddleware from 'redux-saga';
 import { createLogger } from 'redux-logger';
 import rootReducer from '../reducers';
+import mySaga from './sagas';
+
+const sagaMiddleware = createSagaMiddleware();
 
 const logger = createLogger();
 
-export const store = createStore(rootReducer, applyMiddleware(logger));
+export const store = createStore(
+  rootReducer,
+  applyMiddleware(sagaMiddleware, logger),
+);
+
+sagaMiddleware.run(mySaga);

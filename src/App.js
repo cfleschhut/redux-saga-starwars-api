403c59484be76f62c2321f4720f7c278fea528a7
Christian Fleschhut <weltrekordhalter@gmail.com>
2020-01-24 17:52:54 +0100

    Display character names using example api response data

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
+function App(props) {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Star Wars API</h1>
+      <div>
+        {props.starWars.people.results.map((person, i) => (
+          <h4 key={i}>{person.name}</h4>
+        ))}
+      </div>
+
+      <button onClick={props.fetchStarWarsRequest}>Load more</button>
     </div>
   );
 }
